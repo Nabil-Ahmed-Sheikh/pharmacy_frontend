74b8780014ce85a2335a74a45a86447a09b78dec
Nayeem Ahmed <nayeemahmedrincon@gmail.com>
2022-01-23 12:04:14 -0800

    up

--- src/components/Menubar/Menubar.js
+++ src/components/Menubar/Menubar.js
@@ -8,7 +8,37 @@ import {
   FiUsers,
   FiTrendingUp,
   FiGrid,
+  FiTool,
+  FiDollarSign,
 } from "react-icons/fi";
+
+import { 
+  IoBuild,
+  IoBandage,
+  IoLogoStackoverflow 
+
+} from "react-icons/io5";
+
+
+import { 
+  ImStatsBars,
+  ImUsers 
+} from "react-icons/im";
+
+import { 
+   AiFillDollarCircle,
+   AiOutlineShoppingCart,
+   AiOutlineAreaChart
+  
+} from "react-icons/ai";
+ 
+import { AiTwotoneContainer } from "react-icons/ai";
+
+import { GiArrowDunk } from "react-icons/gi";
+import { MdAnchor } from "react-icons/md";
+import { RiBubbleChartLine } from "react-icons/ri";
+
+import { SiDraugiemdotlv } from "react-icons/si";
 
 const MenuItem = Menu.Item;
 const { SubMenu } = Menu;
@@ -32,61 +62,63 @@ const Menubar = ({ collapsed, toggleCollapsed }) => {
         </MenuItem>
 
 
-        <SubMenu key="2" title="HR Admin" icon={<FiUsers />}>
+        <SubMenu key="2" title="HR Admin" icon={<ImUsers />}>
           <MenuItem onClick={() => goto("/user_types")}>User Type</MenuItem>
           <MenuItem onClick={() => goto("/users")}>Users</MenuItem>
         </SubMenu>
 
 
-        <SubMenu key="3" title="Content" icon={<FiUsers />}>
+        <SubMenu key="3" title="Content" icon={<RiBubbleChartLine />}>
           <MenuItem onClick={() => goto("/customer_list")}>Customer List</MenuItem>
           <MenuItem onClick={() => goto("/supplier_list")}>Supplier List</MenuItem>
         </SubMenu>
 
         
-        <SubMenu key="4" title="Medicine" icon={<FiUsers />}>
+        <SubMenu key="4" title="Medicine" icon={<IoBandage />}>
           <MenuItem onClick={() => goto("/add_medicine")}>Add Medicine</MenuItem>
           <MenuItem onClick={() => goto("/medicine_list")}>Medicine List</MenuItem>
         </SubMenu>
 
                 
-        <SubMenu key="5" title="Purchases" icon={<FiUsers />}>
+        <SubMenu key="5" title="Purchases" icon={<AiOutlineShoppingCart />}>
           <MenuItem onClick={() => goto("/add_purchases")}>Add Purchases</MenuItem>
           <MenuItem onClick={() => goto("/purchases_list")}>Purchases List</MenuItem>
         </SubMenu>
                 
-        <SubMenu key="6" title="Sales" icon={<FiUsers />}>
+        <SubMenu key="6" title="Sales" icon={<FiTrendingUp />}>
           <MenuItem onClick={() => goto("/add_sale")}>Add Sale</MenuItem>
           <MenuItem onClick={() => goto("/sales_list")}>Sales List</MenuItem>
         </SubMenu>
                 
-        <SubMenu key="7" title="Collection" icon={<FiUsers />}>
+        <SubMenu key="7" title="Collection" icon={<IoLogoStackoverflow />}>
           <MenuItem onClick={() => goto("/add_collection")}>Add Collection</MenuItem>
           <MenuItem onClick={() => goto("/collection_list")}>Collection List</MenuItem>
 
         </SubMenu>
                 
-        <SubMenu key="8" title="Payment" icon={<FiUsers />}>
+        <SubMenu key="8" title="Payment" icon={<ImStatsBars />}>
           <MenuItem onClick={() => goto("/add_payment")}>Add Payment</MenuItem>
           <MenuItem onClick={() => goto("/payment_list")}>Payment List</MenuItem>
         </SubMenu>
+
+        <MenuItem onClick={() => goto("/sales")} key="9" icon={<GiArrowDunk />} >
+        SalesReturn
+        </MenuItem>
+
+        <MenuItem onClick={() => goto("/sales")} key="10" icon={<MdAnchor />} >
+        StockMatching
+        </MenuItem>
                 
-        <SubMenu key="9" title="SalesReturn" icon={<FiUsers />}>
-          <MenuItem onClick={() => goto("/add_purchases")}>Add Purchases</MenuItem>
-        </SubMenu>
+
                 
-        <SubMenu key="10" title="StockMatching" icon={<FiUsers />}>
-          <MenuItem onClick={() => goto("/add_purchases")}>Add Purchases</MenuItem>
-        </SubMenu>
-                
-        <SubMenu key="11" title="HRManagement" icon={<FiUsers />}>
+        <SubMenu key="11" title="HRManagement" icon={<SiDraugiemdotlv />}>
           <MenuItem onClick={() => goto("/employee_type")}>Employee Type</MenuItem>
           <MenuItem onClick={() => goto("/designation")}>Designation</MenuItem>
           <MenuItem onClick={() => goto("/employee_list")}>Employee List</MenuItem>
           <MenuItem onClick={() => goto("/monthly_salary")}>Monthly Salary</MenuItem>
         </SubMenu>
                 
-        <SubMenu key="12" title="Account" icon={<FiUsers />}>
+        <SubMenu key="12" title="Account" icon={<AiFillDollarCircle />}>
           <MenuItem onClick={() => goto("/expense_list")}>Expense List </MenuItem>
           <MenuItem onClick={() => goto("/others_income")}>Others Income</MenuItem>
           <MenuItem onClick={() => goto("/bank_deposit")}>Bank Deposit </MenuItem>
@@ -94,7 +126,7 @@ const Menubar = ({ collapsed, toggleCollapsed }) => {
           <MenuItem onClick={() => goto("/bank_transfer")}>Bank Transfer </MenuItem>
         </SubMenu>
                 
-        <SubMenu key="13" title="Report" icon={<FiUsers />}>
+        <SubMenu key="13" title="Report" icon={<AiTwotoneContainer />}>
           <MenuItem onClick={() => goto("/cash_statement_report")}>Cash Statement Report</MenuItem>
           <MenuItem onClick={() => goto("/expire_medicine_report")}>Expire Medicine Report</MenuItem>
           <MenuItem onClick={() => goto("/stock_report")}>Stock Report</MenuItem>
@@ -121,7 +153,7 @@ const Menubar = ({ collapsed, toggleCollapsed }) => {
         </SubMenu>
 
 
-        <SubMenu key="14" title="Setting" icon={<FiUsers />}>
+        <SubMenu key="14" title="Setting" icon={<FiTool />}>
           <MenuItem onClick={() => goto("/generic_list")}>Generic List</MenuItem>
           <MenuItem onClick={() => goto("/company_list")}>Company List</MenuItem> 
           <MenuItem onClick={() => goto("/medicine_form")}>Medicine Form</MenuItem>
@@ -136,13 +168,7 @@ const Menubar = ({ collapsed, toggleCollapsed }) => {
         </SubMenu>
 
 
-        <MenuItem
-          onClick={() => goto("/sales")}
-          key="15"
-          icon={<FiTrendingUp />}
-        >
-          Sales
-        </MenuItem>
+
       </Menu>
     </div>
   );
