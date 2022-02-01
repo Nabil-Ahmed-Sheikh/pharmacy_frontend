3024c6fbab73ee1a5eba9aa8e15b4f52b0aa5ad5
Mostak Ahmad <mostak.emo.uss@gmail.com>
2022-02-01 22:03:47 +0600

    add user form

--- src/components/HRAdmin/User/Users.js
+++ src/components/HRAdmin/User/Users.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Button } from "antd";
+import { useSelector, useDispatch } from "react-redux";
+import { getUserType } from "../../../redux/actions/hrAdminActions";
+import AddUserTypeModal from "../../Modal/AddUserTypeModal/AddUserTypeModal";
+import UserTypeList from "../../List/UserTypeList/UserTypeList";
+
+
+import AddUserModal from "../../Modal/AddUserModal/AddUserModal";
 
 const Users = () => {
+
+  const dispatch = useDispatch();
+  const [isVisible, setIsVisible] = useState(false);
+  const [loadToggler, setLoadToggler] = useState(false);
+  const openModal = () => {
+    setIsVisible(true);
+  };
+
+  useEffect(() => {
+    dispatch(getUserType());
+  }, [loadToggler]);
+
+  const { loading, error, message } = useSelector((state) => state.addUserType);
+
   return (
     <div>
       <h2>Users</h2>
+      <Button onClick={openModal}>Add User</Button>
+      <AddUserModal
+        isVisible={isVisible}
+        setIsVisible={setIsVisible}
+        loading={loading}
+        message={message}
+      />
     </div>
   );
 };
